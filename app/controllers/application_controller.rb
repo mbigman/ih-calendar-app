079beb644e3ab7c160195fbaef78ea07a083c713
Maxwell Bigman <maxwellsbigman@gmail.com>
2017-11-29 16:54:37 +0000

    working app!

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+require './config/environment'
+require './app/models/sample_model'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+  
+  post '/results' do
+    day= params[:date]   # String of "mm-dd"
+    periods = say_day(day)
+    # puts "these are the periods: #{periods}"
+    @ih_day=day_check(periods)
+    # puts "Here is the #{@ih_day}"
+    erb :results
+  end
+
+
+end
